1c813cbe1370102924f1857f5549c23544e637a0
Jhester425 <145123142+Jhester425@users.noreply.github.com>
2025-04-10 17:12:50 +0200

    Delete src directory

--- src/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
- $db = mysqli_connect('mysql', 'root', 'root') or
-        die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'crud' ) or die(mysqli_error($db));
-?>
